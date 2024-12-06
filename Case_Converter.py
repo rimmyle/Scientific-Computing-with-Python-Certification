93e24a759128ae35f5d3c707eec8c2cfa60cb363
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2024-12-06 04:11:01 -0800

    added code comments

--- Case_Converter.py
+++ Case_Converter.py
@@ -1,11 +1,8 @@
+# Converts text from camel case to snake cased
+# pascal_or_camel_cased_string: passed string to be converted to snake cased string.
 def convert_to_snake_case(pascal_or_camel_cased_string):
-
-    snake_cased_char_list = [
-        '_' + char.lower() if char.isupper()
-        else char
-        for char in pascal_or_camel_cased_string
-    ]
-
+    # Converts the message to list of words, then converts the case of each word
+    snake_cased_char_list = ['_' + char.lower() if char.isupper() else char for char in pascal_or_camel_cased_string]
     return ''.join(snake_cased_char_list).strip('_')
 
 def main():
