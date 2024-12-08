79884d1c54f17b8082075776c7a3b118db5f349d
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2024-12-08 14:22:46 -0800

    removed calculate spacing method and refactored answer line

--- Arithmetic_Formatter.py
+++ Arithmetic_Formatter.py
@@ -1,44 +1,42 @@
 import re
 
-def calculate_spacing(component, max_space, char):
-    space = ''
-    for _ in range(max_space - len(component)):
-        space += char
-    return space + component
-
-
-
 def arithmetic_arranger(problems, show_answers=False):
-    print(len(problems))
     if len(problems) > 5:
         return 'Error: Too many problems.'
+    
     top = ''
     bottom = ''
     separator = ''
     answers = ''
+
     for problem in problems:
         components = problem.split(' ')
-        if len(components[0]) > 4 or len(components[2]) > 4:
-            return 'Error: Numbers cannot be more than four digits.'
         operation = components[1]
-        if not re.match(r'[+-]', operation):
-            return "Error: Operator must be '+' or '-'."
-        if not components[0].isnumeric() or not components[2].isnumeric():
-            return "Error: Numbers must only contain digits."
-        
         spacing = max(len(components[0]), len(components[2])) + 2
 
-        top += calculate_spacing(components[0], spacing, ' ') + '    '
+        if len(components[0]) > 4 or len(components[2]) > 4:
+            return 'Error: Numbers cannot be more than four digits.'
+        
+        if not re.match(r'[+-]', operation):
+            return "Error: Operator must be '+' or '-'."
+        
+        if not ''.join(components[0] + components[2]).isnumeric():
+            return "Error: Numbers must only contain digits."
+        
+        top += ' ' * (spacing - len(components[0])) + components[0] + '    '
 
-        bottom += operation + calculate_spacing(components[2], spacing - 1, ' ') + '    '
+        bottom += operation + ' ' * ((spacing - 1) - len(components[0])) + components[0] + '    '
 
-        separator += calculate_spacing('', spacing, '-') + '    '
+        separator += '-' * spacing + '    '
 
-        if operation == '+':
-            answers += calculate_spacing(str(int(components[0]) + int(components[2])), spacing, ' ') + '    '
-        else:
-            answers += calculate_spacing(str(int(components[0]) - int(components[2])), spacing, ' ') + '    '
-
+        match operation:
+            case '+':
+                answer = str(int(components[0]) + int(components[2]))
+                answers += ' ' * (spacing - len(answer)) + answer + '    '
+            case _:
+                answer = str(int(components[0]) - int(components[2]))
+                answers += ' ' * (spacing - len(answer)) + answer + '    '
+        
     result = top.rstrip(top[-4]) + '\n' + bottom.rstrip(bottom[-4]) + '\n' + separator.rstrip(separator[-4])
 
     if show_answers:
@@ -46,4 +44,4 @@ def arithmetic_arranger(problems, show_answers=False):
 
     return result
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True)}')
